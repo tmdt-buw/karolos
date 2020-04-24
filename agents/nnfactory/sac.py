Fix action bound in log_prob calculation

agents/nnfactory/sac.py
@@ -44,12 +44,14 @@ class Critic(nn.Module):
         self.operators = nn.ModuleList([
             Flatten(),
             nn.Linear(in_dim + action_dim, hidden_dim),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.Dropout(.2)
         ])
 
         for l in range(num_layers_linear_hidden - 1):
             self.operators.append(nn.Linear(hidden_dim, hidden_dim))
-            self.operators.append(nn.ReLU())
+            self.operators.append(nn.ReLU()),
+            self.operators.append(nn.Dropout(.2))
 
         self.operators.append(nn.Linear(hidden_dim, 1))
 
@@ -72,20 +74,21 @@ class Policy(nn.Module):
         assert len(action_dim) == 1
 
         self.in_dim = np.product(in_dim)
-        self.hidden_dim = np.product(hidden_dim)
         self.action_dim = np.product(action_dim)
+        self.hidden_dim = hidden_dim
 
         # device is initialized by agents Class
         self.operators = nn.ModuleList([
             Flatten(),
             nn.Linear(self.in_dim, self.hidden_dim),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.Dropout(.2)
         ])
 
         for l in range(num_layers_linear_hidden - 1):
             self.operators.append(nn.Linear(hidden_dim, hidden_dim))
             self.operators.append(nn.ReLU())
-            # self.operators.append(nn.Dropout(0.2))
+            self.operators.append(nn.Dropout(0.2))
 
         self.operators.append(nn.Linear(self.hidden_dim, 2 * self.action_dim))
 
@@ -110,17 +113,30 @@ class Policy(nn.Module):
             covariance = torch.diag_embed(std)
             m = MultivariateNormal(mean, covariance)
             action_base = m.sample()
+            action = action_base.tanh()
+
             log_prob = m.log_prob(action_base)
             log_prob.unsqueeze_(-1)
 
-            action = action_base.tanh()
-
             # According to "Soft Actor-Critic" (Haarnoja et. al) Appendix C
-            action_bound_compensation = torch.log(1. - action.tanh().pow(2) + 1e-6)
+            action_bound_compensation = torch.log(1. - action.pow(2) + 1e-6)
             action_bound_compensation = action_bound_compensation.sum(dim=-1, keepdim=True)
             log_prob.sub_(action_bound_compensation)
 
         return action, log_prob
+
+    def get_activations(self, state):
+        x = state
+
+        activations = []
+
+        for operator in self.operators:
+            x = operator(x)
+
+            if type(operator) == nn.ReLU:
+                activations.append(x)
+
+        return activations
 
 
 if __name__ == '__main__':

agents/sac.py
@@ -122,7 +122,7 @@ class AgentSAC:
         predicted_q2 = self.critic_2(states, actions)
 
         predicted_action, log_prob = self.policy(states, deterministic=False)
-        predicted_next_action, new_log_prob = self.policy(next_states, deterministic=False)
+        predicted_next_action, next_log_prob = self.policy(next_states, deterministic=False)
 
         # normalize with batch mean std
         # rewards = self.reward_scale * (rewards - rewards.mean(dim=0)) / (
@@ -142,7 +142,7 @@ class AgentSAC:
         target_critic_min = torch.min(
             self.target_critic_1(next_states, predicted_next_action),
             self.target_critic_2(next_states, predicted_next_action))
-        target_critic_min -= self.alpha * new_log_prob
+        target_critic_min.sub_(self.alpha * next_log_prob)
         target_q_value = rewards + (1 - dones) * self.gamma * target_critic_min
         q_val_loss1 = self.criterion_critic_1(predicted_q1,
                                               target_q_value.detach())