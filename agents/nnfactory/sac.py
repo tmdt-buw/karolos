Fix log prob computation
MultivariateNormal.log_prob returns the unscaled probability, which leads to log_prob being > 0 for small variances.
Therefore actions are sampled from the normal distribution and transformed, which results in log_prob < 0.

agents/nnfactory/sac.py
@@ -110,18 +110,22 @@ class Policy(nn.Module):
         else:
             log_std = self.std_clamp(log_std)
             std = log_std.exp()
-            covariance = torch.diag_embed(std.pow(2))
-            m = MultivariateNormal(mean, covariance)
-            action_base = m.sample()
-            action = action_base.tanh()
 
-            log_prob = m.log_prob(action_base)
+            # Draw from Normal so that log_prob is normalized
+            normal = MultivariateNormal(torch.zeros_like(mean),
+                                        torch.diag_embed(torch.ones_like(std)))
+            z = normal.sample()
+
+            log_prob = normal.log_prob(z)
             log_prob.unsqueeze_(-1)
 
-            # According to "Soft Actor-Critic" (Haarnoja et. al) Appendix C
-            action_bound_compensation = torch.log(1. - action.pow(2) + 1e-6)
-            action_bound_compensation = action_bound_compensation.sum(dim=-1,
-                                                                      keepdim=True)
+            action_base = mean + std * z
+            action = torch.tanh(action_base)
+
+            action_bound_compensation = torch.log(
+                1. - action.pow(2) + np.finfo(float).eps).sum(dim=1,
+                                                              keepdim=True)
+
             log_prob.sub_(action_bound_compensation)
 
         return action, log_prob
@@ -160,7 +164,8 @@ if __name__ == '__main__':
     log_probs = []
 
     for ii in range(1_000):
-        action, log_prob = policy(torch.FloatTensor([[1, 1, 1, 1]]).to(device), False)
+        action, log_prob = policy(torch.FloatTensor([[1, 1, 1, 1]]).to(device),
+                                  False)
 
         # print(action.shape, log_prob)
         actions.append(action[0].cpu().detach().numpy())

agents/sac.py
@@ -122,7 +122,8 @@ class AgentSAC:
         predicted_q2 = self.critic_2(states, actions)
 
         predicted_action, log_prob = self.policy(states, deterministic=False)
-        predicted_next_action, next_log_prob = self.policy(next_states, deterministic=False)
+        predicted_next_action, next_log_prob = self.policy(next_states,
+                                                           deterministic=False)
 
         # normalize with batch mean std
         # rewards = self.reward_scale * (rewards - rewards.mean(dim=0)) / (
@@ -156,8 +157,9 @@ class AgentSAC:
         self.optimizer_critic_2.step()
 
         # Training policy
-        predicted_new_q_val = torch.min(self.critic_1(states, predicted_action),
-                                        self.critic_2(states, predicted_action))
+        predicted_new_q_val = torch.min(
+            self.critic_1(states, predicted_action),
+            self.critic_2(states, predicted_action))
         loss_policy = (self.alpha * log_prob - predicted_new_q_val).mean()
 
         self.optimizer_policy.zero_grad()
@@ -255,14 +257,6 @@ class AgentSAC:
         action = action.detach().cpu().numpy()
 
         action = action.clip(self.action_space.low, self.action_space.high)
-
-        return action
-
-    def random_action(self, states):
-
-        action_dim = (len(states),) + tuple(self.action_dim)
-
-        action = np.random.rand(*action_dim) * 2 - 1
 
         return action
 

refactor.py
@@ -5,109 +5,48 @@ import numpy as np
 
 epsilon = 1e-06
 
-
-def old_code(mean, std):
-    normal = Normal(0, 1)
-    z = normal.sample()
-
-    action = torch.tanh(mean + std * z)
-
-    # print(torch.log(1. - action.pow(2) + 1e-6).sum(dim=1, keepdim=True))
-
-    action_bound_compensation = torch.log(1. - action.pow(2) + np.finfo(float).eps).sum(dim=1, keepdim=True)
-
-    log_prob = Normal(mean, std).log_prob(mean + std * z)
-    log_prob -= action_bound_compensation
-
-    # print(action.shape, log_prob.shape)
-
-    return action, log_prob.exp()
-
-
-def new_code(mean, std):
-    covariance = torch.diag_embed(std.pow(2))
-    m = MultivariateNormal(mean, covariance)
-    action_base = m.sample()
-    action = action_base.tanh()
-
-    log_prob = m.log_prob(action_base)
-    log_prob.unsqueeze_(-1)
-
-    # According to "Soft Actor-Critic" (Haarnoja et. al) Appendix C
-    action_bound_compensation = torch.log(1. - action.pow(2) + np.finfo(float).eps)
-    action_bound_compensation = action_bound_compensation.sum(dim=-1,
-                                                              keepdim=True)
-    # log_prob = log_prob - action_bound_compensation
-
-    return action, torch.zeros_like(log_prob.exp())
-
-
 old_actions = []
-new_actions = []
+actions = []
 old_log_prob = []
-new_log_prob = []
+log_probs = []
 
 mean = torch.Tensor([[0, 0], [1, 1], [2, 2]])
-std = torch.diag_embed(torch.Tensor([[1., 1.], [.1, .1], [10, 10]]))
-
-#print(mean.shape, std.shape)
-
-m = MultivariateNormal(mean, std)
-action_base = m.sample()
-# action_base = action_base.reshape(mean.shape)
-log_prob = m.log_prob(action_base)
-action = action_base.tanh()
-
-# According to "Soft Actor-Critic" (Haarnoja et. al) Appendix C
-action_bound_compensation = torch.log(1. - action.tanh().pow(2) + 1e-6)
-#print(action_bound_compensation.shape)
-action_bound_compensation = action_bound_compensation.sum(dim=-1)
-#print(action_bound_compensation.shape)
-log_prob -= action_bound_compensation
-
-mean = torch.Tensor([[0, 0], [1, 1], [2, 2]])
-std = torch.Tensor([[.1, .0001], [.1, .1], [.01, .01]])
-
-#mean = torch.Tensor([[0, 0]])
-#std = torch.Tensor([[.1]])
-
-print(mean.shape, std.shape)
+std = torch.Tensor([[.1, 1e-7], [.1, .1], [.01, .01]])
 
 for ii in range(1_000):
-    action, log_prob = old_code(mean, std)
-    # print(action)
-    old_actions.append(action[0].numpy())
-    old_log_prob.append(log_prob[0].numpy())
+    normal = MultivariateNormal(torch.zeros_like(mean),
+                                torch.diag_embed(torch.ones_like(std)))
+    z = normal.sample()
 
-    action, log_prob = new_code(mean, std)
-    # print(action)
-    # print(log_prob)
-    new_actions.append(action[0].numpy())
-    new_log_prob.append(log_prob[0].numpy())
+    log_prob = normal.log_prob(z)
+    log_prob.unsqueeze_(-1)
 
-old_actions = np.array(old_actions)
-new_actions = np.array(new_actions)
-old_log_prob = np.array(old_log_prob)
-new_log_prob = np.array(new_log_prob)
-# print(len(old_actions))
-# print(len(new_actions))
-#
+    action_base = mean + std * z
+    action = torch.tanh(action_base)
+
+    action_bound_compensation = torch.log(
+        1. - action.pow(2) + np.finfo(float).eps).sum(dim=1, keepdim=True)
+
+    log_prob.sub_(action_bound_compensation)
+    # log_prob = log_prob.exp().sqrt()
+
+    actions.append(action[0].numpy())
+    log_probs.append(log_prob[0].numpy())
+
+actions = np.array(actions)
+log_probs = np.array(log_probs)
+
 plt.figure()
-plt.hist(old_actions[:,0], alpha=0.5)  # , range=[-1,1])
-plt.hist(new_actions[:,0], alpha=0.5)  # , range=[-1,1])
+plt.hist(actions[:, 0], alpha=0.5)  # , range=[-1,1])
 
 plt.figure()
-plt.hist(old_log_prob[:,0], alpha=0.5)  # , range=[-1,1])
-plt.hist(new_log_prob[:,0], alpha=0.5)  # , range=[-1,1])
-
+plt.hist(log_probs[:, 0], alpha=0.5)  # , range=[-1,1])
 
 from scipy.stats import gaussian_kde
-xy = np.vstack([new_actions[:,0],new_actions[:,1]])
-z = gaussian_kde(xy)(xy)
-
 
 plt.figure()
-plt.scatter(old_actions[:,0],old_actions[:,1])
-plt.figure()
-plt.scatter(new_actions[:,0],new_actions[:,1], c=z, s=100, edgecolor='')
+# xy = np.vstack([actions[:, 0], actions[:, 1]])
+# z = gaussian_kde(xy)(xy)
+sc = plt.scatter(actions[:, 0], actions[:, 1], c=log_probs, s=100, edgecolor='')
+plt.colorbar(sc)
 plt.show()