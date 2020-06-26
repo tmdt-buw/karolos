da0779cc6a12f99851c485a0976eb286d175d287
Thun,Timo <timo.thun@ima-ifu.rwth-aachen.de>
2020-06-26 02:07:24 +0200

    added different activation funcs to critics

--- agents/nnfactory/sac.py
+++ agents/nnfactory/sac.py
@@ -49,12 +49,17 @@ class Critic(nn.Module):
             if layer == 'linear':
                 self.operators.append(nn.Linear(current_layer_size, params))
                 current_layer_size = params
-
             elif layer == 'relu':
                 assert params is None, 'No argument for ReLU please'
                 self.operators.append(nn.ReLU())
+            elif layer == 'selu':
+                assert params is None, 'No argument for SeLU please'
+                self.operators.append(nn.SELU())
+            elif layer == 'tanh':
+                assert params is None, 'No argument for Tanh please'
+                self.operators.append(nn.Tanh())
             elif layer == 'gelu':
-                assert params is None, 'No argument for GeLU'
+                assert params is None, 'No argument for GreLU please'
                 self.operators.append(nn.GELU())
             elif layer == 'dropout':
                 self.operators.append(nn.Dropout(params))
