ddc011c1900097aaef6a77af7e590b35a7c20dab
Timo <timothun@web.de>
2021-05-01 15:03:29 +0200

    added seed

--- karolos/utils.py
+++ karolos/utils.py
@@ -1,4 +1,8 @@
+import os
+import random
+
 import numpy as np
+import torch
 
 
 def unwind_dict_values(element):
@@ -21,3 +25,13 @@ def unwind_space_shapes(space):
         shapes += unwind_space_shapes(space_)
 
     return shapes
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
