3de09eb2886a65ffc065445125694801ea73816c
Christian Bitter <bitter@uni-wuppertal.de>
2023-07-06 13:30:48 +0200

    Fix relative import in environments

--- karolos/environments/__init__.py
+++ karolos/environments/__init__.py
@@ -3,10 +3,10 @@ def get_env(env_config):
     environment_name = env_config.pop("name", "robot-task")
 
     if environment_name == "robot-task":
-        from karolos.environments.environment_robot_task import EnvironmentRobotTask
+        from .environment_robot_task import EnvironmentRobotTask
         env = EnvironmentRobotTask(**env_config)
     elif environment_name == "gym":
-        from karolos.environments.environment_gym_wrapper import GymWrapper
+        from .environment_gym_wrapper import GymWrapper
         env = GymWrapper(**env_config)
     else:
         raise ValueError(f"Unknown environment: {environment_name}")
